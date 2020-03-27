15fd409c9794b3e02edf673a63ed7c9377ee8442
Petar <pera.raickovic@gmail.com>
2020-03-27 20:22:31 +0100

    Adding Dockerfile

--- Converter/Converter/API.py
+++ Converter/Converter/API.py
@@ -1,11 +1,8 @@
-
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
-from sqlalchemy import create_engine
 from json import dumps
 import wget
 import os
-import xml.etree.ElementTree as ET
 import json
 from datetime import datetime,timedelta, date
 from FileDownloading import DownloadClass
